eb219e1010469a91e25523898df7295e9e3c4fe7
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-20 14:53:33 -0300

    Adapting components/header to work without react-bootstrap

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
 
 export default function Header(){
 	return(
 	<header className="App-header">
-		<div>
-			Github Lister
-			{Array(7).fill(null).map((obj, key)=>(
-				<React.Fragment
-					key={key}
-				>
-					&nbsp;
-				</React.Fragment>))}
-			<Image
-				roundedCircle
-				src="https://webdesignledger.com/wp-content/uploads/2015/09/00-featured-github-octocat-logo.jpg"
-				height="45"
-			/>	
-		</div>
+		Github Lister
+		{Array(7).fill(null).map((obj, key)=>(
+			<React.Fragment
+				key={key}
+			>
+				&nbsp;
+			</React.Fragment>))}
+		<img
+			alt="octocat logo"
+			className="rounded"
+			src="https://webdesignledger.com/wp-content/uploads/2015/09/00-featured-github-octocat-logo.jpg"
+			height="45"
+		/>	
 	</header>
 	);
 }
