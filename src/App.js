9269bc63e9dd348328a2270d422978bf094f25d1
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-20 14:58:51 -0300

    Added dark theme

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import './App.css';
+import { withStyles } from '@material-ui/core/styles';
 import Header from './components/header';
 import Routes from './routes';
 
-function App() {
+const themes = {
+	dark: {
+		backgroundColor: '#282c34',
+		color: 'white',
+		'& .MuiFormLabel-root': {
+			color: 'white'
+		},
+		'& .MuiFilledInput-underline:before': {
+			borderBottomColor:'green'
+		},
+		'& .MuiFilledInput-underline:hover:before': {
+			borderBottomColor: 'darkgreen'
+		},
+		'& .MuiFilledInput-root': {
+			color: 'white',
+			backgroundColor: '#FFFFFF28'
+		}
+	}
+}
+
+function App({classes}) {
 	return (
-	<div className="App">
+	<div className={"App "+classes.dark}>
 		<Header/>
 		<Routes/>
 	</div>
 	);
 }
 
-export default App;
+export default withStyles(themes)(App);
