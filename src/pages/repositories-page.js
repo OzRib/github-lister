3bc0b9af85f518363b3c336f125390c399123c04
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-21 19:22:41 -0300

    Created repositories-page

---
+++ src/pages/repositories-page.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import axios from 'axios';
+import { Grid } from '@material-ui/core';
+import { 
+	Logo, 
+	RepositoriesHeader, 
+	User,
+	Repositories
+} from '../components';
+
+export default function RepositoriesPage({match:{params}}){
+	const username = params.user
+	const [userdata, setUserdata] = React.useState({})
+	const [repositories, setRepositories] = React.useState([])
+
+	async function searchUser(name){
+		const user = await axios.get(`https://api.github.com/users/${encodeURIComponent(name)}`)
+		setUserdata(user.data)
+
+		const repos = await axios.get(`https://api.github.com/users/${name}/repos`)
+		setRepositories(repos.data)
+	}
+
+	React.useEffect(()=>{
+		searchUser(username)
+	},[username])
+
+	return(
+	<React.Fragment>
+		<Grid 
+			container
+			direction="row"
+			justifyContent="space-between"
+		>
+			<Grid item>
+				<RepositoriesHeader
+					username={username}
+				/>	
+			</Grid>
+			<Grid item>
+				<Logo/>
+			</Grid>
+		</Grid>
+		<Grid container spacing={0}>
+			<Grid 
+				item 
+				lg={4}
+				md={5}
+				container
+				direction="column"
+				alignItems="center"
+			>
+				<User userdata={userdata}/>
+			</Grid>
+			<Grid
+				item
+				lg={8}
+				md={7}
+				container
+				direction="column"
+				alignItems="center"
+			>
+				<Repositories repositories={repositories}/>
+			</Grid>
+		</Grid>
+	</React.Fragment>
+	);
+}
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route } from 'react-router-dom';
-import { Home, SearchPage } from './pages';
+import { Home, RepositoriesPage } from './pages';
 
 export default function Routes(){
 	return(
 	<BrowserRouter>
 		<Route exact path="/" component={Home}/>
-		<Route exact path="/search/:user" component={SearchPage}/>
+		<Route exact path="/repositories/:user" component={RepositoriesPage}/>
 	</BrowserRouter>
 	);
 }
