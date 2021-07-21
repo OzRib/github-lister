01c9ed7238873adf2cd29bcd35d26b8a7934dae6
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-21 19:28:22 -0300

    Added error handling in pages/home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,22 @@
 import Search from '../components/search';
+import React from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default function Home(){
 	const history = useHistory()
 
 	return(
-	<Search 
-		onSearch={user =>{
-			if(user.trim() !== ''){
-				history.push(`/search/${user}`)
-				return true
-			}else{
-				return 'error'
-			}
-		}}
-	/>
+	<React.Fragment>
+		<Search 
+			onSearch={user =>{
+				if(user.trim() !== ''){
+					history.push(`/repositories/${user}`)
+					return true
+				}else{
+					return 'error'
+				}
+			}}
+		/>
+	</React.Fragment>
 	);
 }
