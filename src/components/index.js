bd0a18243340e8538d279f0f56f583decdeffbdf
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-22 19:40:20 -0300

    Updated wrapper of components

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ import User from './user';
 import Repositories from './repositories';
 import Repository from './repository';
 import UserdataTable from './userdata-table';
+import RepositoriesModal from './repositories-modal';
+import RepositorydataTable from './repositorydata-table';
 
 export {
 	Search,
@@ -13,5 +15,7 @@ export {
 	User,
 	Repositories,
 	Repository,
-	UserdataTable
+	UserdataTable,
+	RepositoriesModal,
+	RepositorydataTable
 };
