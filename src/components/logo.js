d593b74b1de8c522c4fc811928ef92ef327ac388
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-23 12:56:09 -0300

    Adding button to toggle theme in components/logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,58 @@
 import React from 'react';
+import { ThemeContext } from '../providers/theme';
+import {
+	Icon,
+	IconButton
+} from '@material-ui/core';
 
 export default function Logo({maxLogo=true}){
-	return(
-	<div className="App-logo">
-		{maxLogo && 
+	const [theme, setTheme] = React.useContext(ThemeContext)
+	const [themeLight, setThemeLight] = React.useState(theme === 'light')
+
+	function spaces(number=0){
+		return (
 		<React.Fragment>
-			Github Lister
-			{Array(7).fill(null).map((obj, key)=>(
+			{Array(number).fill(null).map((obj, key)=>(
 			<React.Fragment
 				key={key}
 			>
 				&nbsp;
-			</React.Fragment>))}
+			</React.Fragment>
+			))}
+		</React.Fragment>
+		);
+	}
+
+	return(
+	<div className="App-logo">
+		<IconButton
+			onClick={()=>{
+				const light = theme === 'light'
+				if(light){
+					setTheme('dark')
+				}else{
+					setTheme('light')
+				}
+				setThemeLight(!light)
+			}}
+		>
+			{themeLight? 
+			(<Icon
+				color="inherit"
+			>
+				brightness_4
+			</Icon>):
+			(<Icon
+				color="inherit"
+			>
+				brightness_7
+			</Icon>)}
+		</IconButton>
+		{spaces(1)}
+		{maxLogo && 
+		<React.Fragment>
+			Github Lister
+			{spaces(7)}
 		</React.Fragment>}
 		<img
 			alt="octocat logo"
