981526a83aa80e9068767c42dbf76455ba15dd7f
OzRib <ozeiasnascimentoribeiro@gmail.com>
2021-07-23 12:54:27 -0300

    Using ThemeContext to define the theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import { ThemeContext } from './providers/theme';
 import { withStyles } from '@material-ui/core/styles';
 import Routes from './routes';
 import themes from './themes';
 
 function App({classes}) {
+	const [theme] = React.useContext(ThemeContext);
+
 	return (
-	<div className={"App "+classes.dark+" "+classes.root}>
+	<div className={"App "+classes[theme]+" "+classes.root}>
 		<Routes/>
 	</div>
 	);
